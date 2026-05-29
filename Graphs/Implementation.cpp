52e76191ca61d8ec521eace6e812aec7be953805
pinakkk <pinakkundu1080@gmail.com>
2026-05-30 00:55:11 +0530

    adjacency list

--- Graphs/Implementation.cpp
+++ Graphs/Implementation.cpp
@@ -6,21 +6,46 @@ int main() {
   int n, m;
   cin >> n >> m;
 
-  // graph is being stored here, all elements initialized as 0
-  vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+  //  //  graph is being stored here, all elements initialized as 0
+  //  // using matrix O(N * N) space
+  //   vector<vector<int>> adj(n + 1, vector<int>(n + 1, 0));
+
+  //   for (int i = 0; i < m; i++) {
+  //     int u, v;
+  //     cin >> u >> v;
+  //     adj[u][v] = 1;
+  //     adj[v][u] = 1;
+  //   }
+
+  //   for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < n; j++) {
+  //     cout << adj[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
+
+  // // using adjacency list O(2 * E);
+  vector<int> adj[n + 1];
 
   for (int i = 0; i < m; i++) {
     int u, v;
     cin >> u >> v;
-    adj[u][v] = 1;
-    adj[v][u] = 1;
+
+    // Undirected
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+
+    // for directed
+    // adj[u].push_back(v);
   }
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cout << adj[i][j] << " ";
+    cout << i << " --> ";
+    for (auto it : adj[i]) {
+      cout << it << " ";
     }
     cout << endl;
   }
+
   return 0;
 }
