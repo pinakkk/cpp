360c1300911d7058278b68d03bd9c0ced6706408
pinakkk <pinakkundu1080@gmail.com>
2026-05-30 22:45:10 +0530

    kth largest element

---
+++ Heaps/kthlargestQ4.cpp
@@ -0,0 +1,39 @@
+// Naive Approach :
+// Add score
+// sort all
+// print kth largest
+
+// Approach : Min heap smallest elements stay at top root
+
+// [ 4, 5, 8 ]
+// if we enter 2 into it, size becomes greater, so we immediately pop 2 from it,
+// so it stays the kth largest which is 3rd largest here;
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+  int k, n;
+  cin >> k >> n;
+  priority_queue<int, vector<int>, greater<int>> pq;
+
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+
+    pq.push(val);
+
+    if (pq.size() > k) {
+      pq.pop();
+    }
+
+    if (pq.size() < k) {
+      cout << -1 << endl;
+    } else {
+      cout << pq.top() << " ";
+    }
+  }
+
+  return 0;
+}
